67ef8d018b806e15a0e803aff42ca59f3c98d508
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-11-03 09:56:17 +0100

    feat: renaming project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mon-projet' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('mon-projet');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mon-projet');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, marie-cecile-artiste-peintre');
   });
 });
