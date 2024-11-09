66098229394890d0f8bdd29af6c990c96a253212
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-11-09 14:12:01 +0100

    chore: add ngxs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,13 @@ import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsFormPlugin } from '@ngxs/form-plugin';
+import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
+import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
+import { provideStore } from '@ngxs/store';
+import { withNgxsWebSocketPlugin } from '@ngxs/websocket-plugin';
 
 import { routes } from './app.routes';
 
@@ -10,5 +17,15 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
+    provideStore([]),
+    provideStore(
+      [],
+      withNgxsReduxDevtoolsPlugin(),
+      withNgxsFormPlugin(),
+      withNgxsLoggerPlugin(),
+      withNgxsRouterPlugin(),
+      withNgxsStoragePlugin({ keys: '*' }),
+      withNgxsWebSocketPlugin()
+    ),
   ],
 };
