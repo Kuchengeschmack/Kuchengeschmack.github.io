084a3b72add99c0243845cdd795bd9a5e1921dff
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-11-11 09:44:28 +0100

    refactor: authGuard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,17 +1,6 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { LoginService } from 'app/views/login/services/login.service';
-import { tap } from 'rxjs';
 
-export const authGuard: CanActivateFn = () => {
-  const router = inject(Router);
-  const loginService = inject(LoginService);
-
-  return loginService.isAuthenticated$.pipe(
-    tap(isAuthenticated => {
-      if (!isAuthenticated) {
-        router.navigate(['login']);
-      }
-    })
-  );
-};
+export const authGuard: CanActivateFn = () =>
+  inject(LoginService).isAuthenticated$;
