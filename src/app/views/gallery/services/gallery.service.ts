6149a12eae7ad889062184bb911b071bb9626c93
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-11-16 09:37:34 +0100

    perf(gallery): render gallery faster

--- src/app/views/gallery/services/gallery.service.ts
+++ src/app/views/gallery/services/gallery.service.ts
@@ -2,11 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { RouterState } from '@ngxs/router-plugin';
 import { Store } from '@ngxs/store';
-import { catchError, map, switchMap, tap } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs';
 
 import { Gallery, JsonData } from '../models/gallery.model';
 
-const url = 'http://localhost:8080/images';
 const dir = 'assets/images/images.json';
 
 @Injectable({
@@ -23,28 +22,20 @@ export class GalleryService {
     return this._store.select(RouterState.url).pipe(
       switchMap(path =>
         this._http
-          .get<Gallery>(url, {
-            params: { url: path || '' },
-          })
+          .get<JsonData>(dir)
           .pipe(
-            catchError(() =>
-              this._http
-                .get<JsonData>(dir)
-                .pipe(
-                  map(
-                    (jsonData: JsonData) =>
-                      new Gallery(
-                        jsonData,
-                        'assets/images' + path?.split('gallery')[1] || ''
-                      )
-                  )
+            map(
+              (jsonData: JsonData) =>
+                new Gallery(
+                  jsonData,
+                  'assets/images' + path?.split('gallery')[1] || ''
                 )
-            ),
-            tap(gallery => {
-              this._gallery.set(gallery);
-            })
+            )
           )
-      )
+      ),
+      tap(gallery => {
+        this._gallery.set(gallery);
+      })
     );
   }
 }
