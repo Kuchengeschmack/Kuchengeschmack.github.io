a60de654f4a2b3df0127b415f6a2bff5d740d92e
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-11-21 12:26:59 +0100

    feat: display current version in footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from 'components/header.component';
 import { Core } from '.';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [...Core, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <header class="header">
+      <app-header></app-header>
+    </header>
+
+    <main>
+      <article>
+        <router-outlet />
+      </article>
+    </main>
+
+    <footer class="footer">
+      <p>v{{version}}</p>
+    </footer>
+  `,
+  styles: `
+    @use 'core/variables.scss';
+
+    .header {
+      width: 100%;
+      min-height: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 1rem;
+      box-sizing: inherit;
+      position: relative;
+    }
+
+    .footer p {
+      text-align: center;
+    }
+  `,
 })
-export class AppComponent {}
+export class AppComponent {
+  public version = version;
+}
