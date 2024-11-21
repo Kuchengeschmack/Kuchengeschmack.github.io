8cde4dd117d7944202f993bd9f8db6ea2d411228
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-11-21 11:47:54 +0100

    refactor: flatten app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,38 @@ import { Core } from '.';
   selector: 'app-root',
   standalone: true,
   imports: [...Core, HeaderComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `
+    <header class="header">
+      <app-header></app-header>
+    </header>
+
+    <main>
+      <article>
+        <router-outlet />
+      </article>
+    </main>
+
+    <footer class="footer">
+      <p></p>
+    </footer>
+  `,
+  styles: `
+    @use 'core/variables.scss';
+
+    .header {
+      width: 100%;
+      min-height: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 1rem;
+      box-sizing: inherit;
+      position: relative;
+    }
+
+    .footer p {
+      text-align: center;
+    }
+  `,
 })
 export class AppComponent {}
