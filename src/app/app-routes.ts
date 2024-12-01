586efa61814ca5cfac7fefda9af5db9311550b5d
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-12-01 02:31:23 +0100

    feat: remove intro page

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,7 +1,6 @@
 import type { Routes } from '@angular/router';
 
 import { Gallery } from 'app/gallery/gallery';
-import { Intro } from 'app/intro/intro';
 import { Piano } from 'app/piano/piano';
 
 export const routes: Routes = [
@@ -9,11 +8,6 @@ export const routes: Routes = [
     path: 'book',
     title: 'Marie-Cécile Caron | Book',
     component: Gallery,
-  },
-  {
-    path: 'intro',
-    title: 'Marie-Cécile Caron | Présentation',
-    component: Intro,
   },
   {
     path: 'piano',
