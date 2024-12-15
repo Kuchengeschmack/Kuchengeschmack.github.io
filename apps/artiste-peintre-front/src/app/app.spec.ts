a10e5d1125f8484ee1da64bab08ea6d48c31f428
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-12-15 10:15:12 +0100

    test: refactor app test

--- apps/artiste-peintre-front/src/app/app.spec.ts
+++ apps/artiste-peintre-front/src/app/app.spec.ts
@@ -3,16 +3,21 @@ import { RouterModule } from '@angular/router';
 import { App } from './app';
 
 describe('App', () => {
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App, RouterModule.forRoot([])],
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  beforeEach(() => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
+    compiled = fixture.nativeElement as HTMLElement;
+  });
+
+  it('should render title', () => {
     expect(compiled.querySelector('h1')?.textContent).toContain(
       'Marie-Cécile Caron, artiste-peintre',
     );
