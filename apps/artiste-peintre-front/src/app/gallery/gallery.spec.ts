a192847d2f17be6fefefa10c2d97b2ff33bda4f7
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-12-16 22:41:34 +0100

    test: define todos

---
+++ apps/artiste-peintre-front/src/app/gallery/gallery.spec.ts
@@ -0,0 +1,7 @@
+describe('Gallery', () => {
+  it.todo('shoud render title');
+
+  it.todo('should render all photo cards');
+
+  it.todo('should adapt columns to window size');
+});
---
+++ apps/artiste-peintre-front/src/app/gallery/photo-card.spec.ts
@@ -0,0 +1,5 @@
+describe('PhotoCard', () => {
+  it.todo('shoud render title');
+
+  it.todo('should render image');
+});
