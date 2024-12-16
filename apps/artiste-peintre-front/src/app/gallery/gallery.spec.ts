445dc9fe760cb1df6f45f3e122af9bb9e935488b
Dorian Réale--Caron <drealecaron@protonmail.com>
2024-12-16 22:46:39 +0100

    test: should render gallery title

--- apps/artiste-peintre-front/src/app/gallery/gallery.spec.ts
+++ apps/artiste-peintre-front/src/app/gallery/gallery.spec.ts
@@ -1,5 +1,24 @@
+import { TestBed } from '@angular/core/testing';
+import { Gallery } from './gallery';
+
 describe('Gallery', () => {
-  it.todo('shoud render title');
+  let compiled: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Gallery],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    const fixture = TestBed.createComponent(Gallery);
+    fixture.detectChanges();
+    compiled = fixture.nativeElement as HTMLElement;
+  });
+
+  it('shoud render title', () => {
+    expect(compiled.querySelector('h1')?.textContent).toContain('Book');
+  });
 
   it.todo('should render all photo cards');
 
